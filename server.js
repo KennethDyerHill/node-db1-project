cc2608c7ce6794411461cdffa851520a429fecd0
KennethDyerHill <52476371+KennethDyerHill@users.noreply.github.com>
2020-04-24 19:58:51 -0400

    Update

--- server.js
+++ server.js
@@ -56,6 +56,7 @@ server.put('/accounts/:id', (req, res) => {
 			res.status(500).json({ message: 'error updating the account' });
 		});
 });
+
 server.delete('/accounts/:id', (req, res) => {
 	db('accounts')
 		.where('id', '=', req.params.id)
