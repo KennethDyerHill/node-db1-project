09632049f87e31d238b04a68d5415d20b918b5f8
KennethDyerHill <52476371+KennethDyerHill@users.noreply.github.com>
2020-04-24 18:53:48 -0400

    Update

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,8 +1,7 @@
 const knex = require("knex");
 
-const knexfile = require("../knexfile.js");
+const configOptions = require("../knexfile.js").development;
 
 // change to "production" and update knexfile.js to use postgres.
-const database = "development";
 
-module.exports = knex(knexfile[database]);
+module.exports = knex(configOptions);
---
+++ server.js
@@ -0,0 +1,76 @@
+const express = require('express');
+
+const db = require('./data/dbConfig.js');
+
+const server = express();
+
+server.use(express.json());
+//Functional
+
+server.get('/accounts', (req, res) => {
+	db('accounts')
+		.then(accounts => {
+			res.status(200).json(accounts);
+		})
+		.catch(error => {
+			res.status(500).json({ message: 'error getting the accounts' });
+		});
+});
+//Functional
+
+server.get('/accounts/:id', (req, res) => {
+	db('accounts')
+		.where({ id: req.params.id })
+		.first()
+		.then(account => {
+			res.status(200).json(account);
+		})
+		.catch(error => {
+			res.status(500).json({ message: 'error getting the accounts' });
+		});
+});
+server.post('/accounts', (req, res) => {
+	const account = req.body;
+	db('accounts')
+		.insert(account, 'id')
+		.then(account => {
+			res.status(200).json(account);
+		})
+		.catch(error => {
+			res.status(500).json({ message: 'error saving the account to the db' });
+		});
+});
+server.put('/accounts/:id', (req, res) => {
+	const changes = req.body;
+	db('accounts')
+		.where('id', '=', req.params.id)
+		.update(changes)
+		.then(count => {
+			if (count > 0) {
+				res.status(200).json(count);
+			} else {
+				res.status(404).json({ message: 'not found' });
+			}
+		})
+		.catch(error => {
+			res.status(500).json({ message: 'error updating the account' });
+		});
+});
+//Functional
+server.delete('/accounts/:id', (req, res) => {
+	db('accounts')
+		.where('id', '=', req.params.id)
+		.del()
+		.then(count => {
+			if (count > 0) {
+				res.status(200).json(count);
+			} else {
+				res.status(404).json({ message: 'not found' });
+			}
+		})
+		.catch(error => {
+			res.status(500).json({ message: 'error removing the post' });
+		});
+});
+
+module.exports = server;
